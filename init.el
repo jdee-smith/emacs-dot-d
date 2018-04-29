3f206ee0c20eee841f904560040e762680d599c6
Jason Smith <24575027+jdee-smith@users.noreply.github.com>
2018-04-28 18:51:21 -0700

    Made some changes to comments.

--- init.el
+++ init.el
@@ -31,13 +31,13 @@
 
 ;; ---- BASIC CUSTOMIZATION ----
 
-(setq inhibit-startup-message t) ;; hide the startup message
-(global-linum-mode t) ;; enable line numbers globally
+(global-linum-mode t) ; enable line numbers globally
+(setq inhibit-startup-message t) ; hide startup message
 
 ;; ---- DOOM THEME STUFF ----
 
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+(setq doom-themes-enable-bold t    ; bold not universally disabled
+      doom-themes-enable-italic t) ; italics not universally disabled
 
 (load-theme 'doom-one t) ; load particular doom theme
 (doom-themes-visual-bell-config) ; enable flashing mode-line on errors
@@ -46,22 +46,17 @@
 ;; ---- PYTHON CONFIGURATION ----
 
 (elpy-enable)
-;; (elpy-use-ipython)
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i --simple-prompt")
 
-;; use flycheck with elpy
+;; flycheck with elpy
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-;; autopep8 on save
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
-;; use column-enforce-mode with elpy
-(require 'column-enforce-mode)
-(add-hook 'elpy-mode-hook 'column-enforce-mode)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save) ; autopep8 on save
+(add-hook 'elpy-mode-hook 'column-enforce-mode) ; column-enforce-mode with elpy
 
 ;; ---- CONDA VIRTUAL ENV STUFF ----
 
@@ -75,11 +70,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-command "/usr/local/bin/pandoc") ; point to location of Markdown processor
-)
-(custom-set-faces
+ '(markdown-command "/usr/local/bin/pandoc") ; point to Markdown processor
+ (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ ))
